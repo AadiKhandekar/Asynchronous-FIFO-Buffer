8eb1572a0deda38c78dac08c002a1c15d8970d05
AadiKhandekar <157942020+AadiKhandekar@users.noreply.github.com>
2026-04-05 02:32:55 +0530

    Update wrptr__handle.v

--- wrptr__handle.v
+++ wrptr__handle.v
@@ -19,15 +19,6 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-//-------------DESCRIPTION---------------------------
-// This is a FIFO write pointer Handle module with full flag.
-//--------------------------------------------------
-
-//-------------PARAMETERS---------------------------
-// ADDR_SIZE: Size of the address bus
-//--------------------------------------------------
-
 module wptr_handle #(parameter ADDR_SIZE = 4)(
     output reg wfull,                   // Full flag
     output [ADDR_SIZE-1:0] waddr,       // Write address
@@ -52,13 +43,7 @@ module wptr_handle #(parameter ADDR_SIZE = 4)(
     assign wbin_next = wbin + (winc & ~wfull);       // Increment the write pointer if not full
     assign wgray_next = (wbin_next>>1) ^ wbin_next;    // Convert binary to gray code
 
-    // Check if the FIFO is full
-    //------------------------------------------------------------------
-    // Simplified version of the three necessary full-tests:
-    // assign wfull_val=((wg_next[ADDR_SIZE] !=wq2_rptr[ADDR_SIZE] ) &&
-    // (wg_next[ADDR_SIZE-1] !=wq2_rptr[ADDR_SIZE-1]) &&
-    // (wg_next[ADDR_SIZE-2:0]==wq2_rptr[ADDR_SIZE-2:0]));
-    //------------------------------------------------------------------
+   
     assign wfull_val = (wgray_next=={~wq2_rptr[ADDR_SIZE:ADDR_SIZE-1], wq2_rptr[ADDR_SIZE-2:0]});
 
     always @(posedge wclk or negedge wrst_n) begin
@@ -68,13 +53,3 @@ module wptr_handle #(parameter ADDR_SIZE = 4)(
             wfull <= wfull_val; // Update the full flag
     end
 endmodule
-
-// ---------------------------EXPLANATION---------------------------------
-// The module implements a write pointer for a FIFO with a full flag.
-// The write pointer is implemented in gray code to avoid glitches when
-// transitioning between clock domains. The write pointer is incremented based
-// on the write increment signal and the full flag. The full flag is set
-// when the write pointer is equal to the read pointer, indicating that
-// the FIFO is full. The write pointer and full flag are updated on each
-// clock cycle, and the write address is calculated from the write pointer.
-// -----------------------------------------------------------------------
